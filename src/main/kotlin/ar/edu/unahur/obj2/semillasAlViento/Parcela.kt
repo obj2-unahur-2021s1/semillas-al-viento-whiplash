59e2b698f7650477ac46b0e5736a1efdd7de9bb7
msunh <msalto.unh@gmail.com>
2021-05-04 22:31:51 -0300

    Se agregaron los comentarios solicitados

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
@@ -2,44 +2,51 @@ package ar.edu.unahur.obj2.semillasAlViento
 
 class Parcela(val ancho: Int, val largo: Int, val horasSolPorDia: Int) {
   val plantas = mutableListOf<Planta>()
-  var cantidadPlantas = 0
+  var cantidadPlantas = 0 //REDUNDANCIA // MUTACION CONTROLADA = no necesito esta variable para obtener la cantidad de plantas, es algo que le puedo preguntar a la lista de plantas con un método en vez de esta variable
 
-  fun superficie() = ancho * largo
+  fun superficie() = ancho * largo //SIMPLICIDAD = el metodo resuelve el tema de la superficie , pero no se usa
+
   fun cantidadMaximaPlantas() =
-    if (ancho > largo) ancho * largo / 5 else ancho * largo / 3 + largo
+    if (ancho > largo) ancho * largo / 5 else ancho * largo / 3 + largo // ABSTRACCION / REUSABILIDAD= se podria haber utilizado el resultado del método superficie() para calcular ancho*largo
+    //if (ancho > largo) this.superficie() /5 else this.superficie() / 3 + largo
 
-  fun plantar(planta: Planta) {
-    if (cantidadPlantas == this.cantidadMaximaPlantas()) {
+  //fun cantidadDePLantas() = plantas.count() //MUTACION CONTROLADA = este método no está , pero sería ideal antes que la variable cantidadPlantas
+
+  fun plantar(planta: Planta) {  //ROBUSTEZ ?? devería arrojar error al fallar
+    if (cantidadPlantas == this.cantidadMaximaPlantas()) {// se podría haber resuelto de forma más simple
       println("Ya no hay lugar en esta parcela")
     } else if (horasSolPorDia > planta.horasDeSolQueTolera() + 2) {
       println("No se puede plantar esto acá, se va a quemar")
     } else {
       plantas.add(planta)
-      cantidadPlantas += 1
+      cantidadPlantas += 1 // REDUNDANCIA = No hace falta sumar cada vez que se agrega una planta a la parcela , se le debería preguntar a un método
     }
   }
+
+  //fun tieneComplicaciones() // este método es de parcela pero esta en planta
+
 }
 
-class Agricultora(val parcelas: MutableList<Parcela>) {
-  var ahorrosEnPesos = 20000
+class Agricultora(val parcelas: MutableList<Parcela>) { // MINIMIZAR MUTABILIDAD = si "por lo tanto cuando se crea una agricultora se configura con todas las parcelas que tiene." no hace falta que sea una lista mutable, porque no cambian
+  var ahorrosEnPesos = 20000  // YAGNI = esta variable no se necesita, ni se pidio, dado que la tierra no puede ser ni vendida ni comprada , por lo tanto no necesita esta implementacion
 
   // Suponemos que una parcela vale 5000 pesos
-  fun comprarParcela(parcela: Parcela) {
-    if (ahorrosEnPesos >= 5000) {
+  fun comprarParcela(parcela: Parcela) { // YAGNI = este método no se necesita, ni se pidio, dado que la tierra no puede ser ni vendida ni comprada , por lo tanto no necesita esta implementacion
+    if (ahorrosEnPesos >= 5000) { //
       parcelas.add(parcela)
       ahorrosEnPesos -= 5000
     }
   }
 
-  fun parcelasSemilleras() =
+  fun parcelasSemilleras() = //KISS = se puede simplificar
     parcelas.filter {
       parcela -> parcela.plantas.all {
         planta -> planta.daSemillas()
       }
     }
 
-  fun plantarEstrategicamente(planta: Planta) {
-    val laElegida = parcelas.maxBy { it.cantidadMaximaPlantas() - it.cantidadPlantas }!!
+  fun plantarEstrategicamente(planta: Planta) { // SE PODRIA HABER RESUELTO DE FORMA MAS SIMPLE
+    val laElegida = parcelas.maxBy { it.cantidadMaximaPlantas() - it.cantidadPlantas }!! //MUTACION // ACOPLAMIENTO = it.cantidadPlantas accede a una variable de la parcela , lo que no es recomendado
     laElegida.plantas.add(planta)
   }
 }
--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
@@ -1,22 +1,25 @@
 package ar.edu.unahur.obj2.semillasAlViento
 
 abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {
-  fun esFuerte() = this.horasDeSolQueTolera() > 10
+  fun esFuerte() = this.horasDeSolQueTolera() > 10     //SIMPLICIDAD = resuelve lo solicitado de forma concreta y simple
 
-  fun parcelaTieneComplicaciones(parcela: Parcela) =
-    parcela.plantas.any { it.horasDeSolQueTolera() < parcela.horasSolPorDia }
+  fun parcelaTieneComplicaciones(parcela: Parcela) =   // (DES)ACOPLAMIENTO = este método deberia ser de parcela y no de la planta
+    parcela.plantas.any { it.horasDeSolQueTolera() < parcela.horasSolPorDia } //(DES)ACOPLAMIENTO = este método accede a la lista de plantas de la parcela, esta acoplado a la implementacion de parcela, si cambia el código esto ya no sirve
 
   abstract fun horasDeSolQueTolera(): Int
   abstract fun daSemillas(): Boolean
 }
 
+//SIMPLICIDAD
 class Menta(anioObtencionSemilla: Int, altura: Float) : Planta(anioObtencionSemilla, altura) {
   override fun horasDeSolQueTolera() = 6
   override fun daSemillas() = this.esFuerte() || altura > 0.4
 }
 
+//COHESION = en este caso la Soja y la soja transgénica son una sola clase, no es correcto y no es cohesivo, se apunta que esta clase resuelva todos los problemas de otra subclase
+//FLEXIBILIDAD = la implementacion no es flexible dado que si se agrega un cambio afecta a las dos tipos de planta cosa que no sería necesario si se hubieran implementado correctamente
 class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean) : Planta(anioObtencionSemilla, altura) {
-  override fun horasDeSolQueTolera(): Int  {
+  override fun horasDeSolQueTolera(): Int  {  //COHESION = hace demasiadas cosas y podrian ser menos cosas
     // ¡Magia de Kotlin! El `when` es como un `if` pero más poderoso:
     // evalúa cada línea en orden y devuelve lo que está después de la flecha.
     val horasBase = when {
@@ -28,12 +31,14 @@ class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean)
     return if (esTransgenica) horasBase * 2 else horasBase
   }
 
+  // override fun daSemillas() = False // MUTACIONES CONTROLADAS = podria ser un método solo de sojaTransgenica y no un atributo
+
 
   override fun daSemillas(): Boolean  {
     if (this.esTransgenica) {
       return false
     }
 
-    return this.esFuerte() || (this.anioObtencionSemilla > 2007 && this.altura > 1)
+    return this.esFuerte() || (this.anioObtencionSemilla > 2007 && this.altura > 1)  //this.altura > 1 = se podria crear un metodo que resuelva esto y se reutilice cada vez que se necesita
   }
 }
