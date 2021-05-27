47a6ca99c96b34bdc353fe28e6251ea7aafeb4bb
msunh <msalto.unh@gmail.com>
2021-05-27 19:08:47 -0300

    Test de Parcelas

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
@@ -1,6 +1,6 @@
 package ar.edu.unahur.obj2.semillasAlViento
 
-abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {
+abstract class Planta(val anioObtencionSemilla: Int, var altura: Double) {
   fun esFuerte() = this.horasDeSolQueTolera() > 10     //SIMPLICIDAD = resuelve lo solicitado de forma concreta y simple
 
   fun parcelaTieneComplicaciones(parcela: Parcela) =   // (DES)ACOPLAMIENTO = este método deberia ser de parcela y no de la planta
@@ -11,14 +11,15 @@ abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {
 }
 
 //SIMPLICIDAD
-class Menta(anioObtencionSemilla: Int, altura: Float) : Planta(anioObtencionSemilla, altura) {
+class Menta(anioObtencionSemilla: Int, altura: Double) : Planta(anioObtencionSemilla, altura) { // se refactoriza de Float A double para poder correr Test
   override fun horasDeSolQueTolera() = 6
   override fun daSemillas() = this.esFuerte() || altura > 0.4
 }
 
 //COHESION = en este caso la Soja y la soja transgénica son una sola clase, no es correcto y no es cohesivo, se apunta que esta clase resuelva todos los problemas de otra subclase
 //FLEXIBILIDAD = la implementacion no es flexible dado que si se agrega un cambio afecta a las dos tipos de planta cosa que no sería necesario si se hubieran implementado correctamente
-class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean) : Planta(anioObtencionSemilla, altura) {
+
+class Soja(anioObtencionSemilla: Int, altura: Double, val esTransgenica: Boolean) : Planta(anioObtencionSemilla, altura) { //Se Refactoriza la altura de float a doble para poder realizar test
   override fun horasDeSolQueTolera(): Int  {  //COHESION = hace demasiadas cosas y podrian ser menos cosas
     // ¡Magia de Kotlin! El `when` es como un `if` pero más poderoso:
     // evalúa cada línea en orden y devuelve lo que está después de la flecha.
---
+++ src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/ParcelaTest.kt
@@ -0,0 +1,63 @@
+package ar.edu.unahur.obj2.semillasAlViento
+
+import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.shouldBe
+
+class ParcelaTest : DescribeSpec( {
+
+    describe("Una parcela"){
+        val unaParcela = Parcela(20,1,8)
+        val plantaDeSoja1 = Soja(1998,2.0,false)
+        val plantaDeSoja2 = Soja(1997,3.0,true)
+        val plantaDeSoja3 = Soja(1994,2.0,false)
+        val plantaDeSoja4 = Soja(1996,2.0,true)
+        val plantaDeSoja5 = Soja(1996,2.3,false)
+        it("Conocer superficie de una parcela"){
+
+        unaParcela.superficie().shouldBe(20)
+
+        }
+
+        it("cantidad maxima de plantas que tolera"){
+            unaParcela.cantidadMaximaPlantas().shouldBe(4)
+        }
+
+        it ("saber si tiene complicaciones"){
+            unaParcela.plantar(plantaDeSoja1)
+            unaParcela.plantar(plantaDeSoja2)
+            unaParcela.plantar(plantaDeSoja3)
+            unaParcela.plantar(plantaDeSoja4)
+            plantaDeSoja1.parcelaTieneComplicaciones(unaParcela).shouldBeFalse()
+
+        }
+        /*
+        Este test no se puede correr porque en el código no devuelve un error sino que en su lugar imprime un mensaje de error
+        it("una parcela tira error si quiere agregar una quinta planta de soja"){
+            unaParcela.plantar(plantaDeSoja1)
+            unaParcela.plantar(plantaDeSoja2)
+            unaParcela.plantar(plantaDeSoja3)
+            unaParcela.plantar(plantaDeSoja4)
+
+            shouldThrowAny {
+                unaParcela.plantar(plantaDeSoja5)
+            }
+
+        }
+        */
+
+        
+
+
+
+
+
+
+    }
+
+
+
+
+
+})
